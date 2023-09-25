c324004b2ca66dd626b8237f4205272330eac924
GArdennes <kevin.afachao@gmail.com>
2023-09-25 03:11:54 -0700

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - algorithm that sorts using selection sort algorithm
+ * @array: integer array
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx, temp;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
+}
